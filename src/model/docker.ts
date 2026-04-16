93259a79b14499b5c6cd562e338c3aa2704ce3dd
GA_IA <142288507+GA-IA@users.noreply.github.com>
2026-04-16 20:01:32 +0700

    Update docker.ts

--- src/model/docker.ts
+++ src/model/docker.ts
@@ -109,26 +109,32 @@ class Docker {
     return `docker run \
             --workdir ${dockerWorkspacePath} \
             --rm \
-            ${ImageEnvironmentFactory.getEnvVarString(parameters)} \
-            --env BEE_CACHE_DIRECTORY=c:${dockerWorkspacePath}/Library/bee_cache \
-            --env GITHUB_WORKSPACE=c:${dockerWorkspacePath} \
+            ${ImageEnvironmentFactory.getEnvVarString(parameters, additionalVariables)} \
+            --env GITHUB_WORKSPACE=${dockerWorkspacePath} \
+            --env GIT_CONFIG_EXTENSIONS \
             ${gitPrivateToken ? `--env GIT_PRIVATE_TOKEN="${gitPrivateToken}"` : ''} \
-            --volume "${workspace}":"c:${dockerWorkspacePath}" \
-            --volume "${githubHome}":"C:/githubhome" \
-            --volume "c:/regkeys":"c:/regkeys" \
-            --volume "C:/Program Files/Microsoft Visual Studio":"C:/Program Files/Microsoft Visual Studio" \
-            --volume "C:/Program Files (x86)/Microsoft Visual Studio":"C:/Program Files (x86)/Microsoft Visual Studio" \
-            --volume "C:/Program Files (x86)/Windows Kits":"C:/Program Files (x86)/Windows Kits" \
-            --volume "C:/ProgramData/Microsoft/VisualStudio":"C:/ProgramData/Microsoft/VisualStudio" \
-            --volume "${actionFolder}/default-build-script":"c:/UnityBuilderAction" \
-            --volume "${actionFolder}/platforms/windows":"c:/steps" \
-            --volume "${actionFolder}/unity-config":"C:/ProgramData/Unity/config" \
-            --volume "${actionFolder}/BlankProject":"c:/BlankProject" \
+            ${sshAgent ? '--env SSH_AUTH_SOCK=/ssh-agent' : ''} \
+            --volume "${githubHome}":"/root:z" \
+            --volume "${githubWorkflow}":"/github/workflow:z" \
+            --volume "${workspace}":"${}:z" \
+            --volume "${actionFolder}/default-build-script:/UnityBuilderAction:z" \
+            --volume "${actionFolder}/platforms/ubuntu/steps:/steps:z" \
+            --volume "${actionFolder}/platforms/ubuntu/entrypoint.sh:/entrypoint.sh:z" \
+            --volume "${actionFolder}/unity-config:/usr/share/unity3d/config/:z" \
+            --volume "${actionFolder}/BlankProject":"/BlankProject:z" \
             --cpus=${dockerCpuLimit} \
             --memory=${dockerMemoryLimit} \
-            --isolation=${dockerIsolationMode} \
+            ${sshAgent ? `--volume ${sshAgent}:/ssh-agent` : ''} \
+            ${
+              sshAgent && !sshPublicKeysDirectoryPath
+                ? '--volume /home/runner/.ssh/known_hosts:/root/.ssh/known_hosts:ro'
+                : ''
+            } \
+            ${sshPublicKeysDirectoryPath ? `--volume ${sshPublicKeysDirectoryPath}:/root/.ssh:ro` : ''} \
+            ${entrypointBash ? `--entrypoint ${commandPrefix}` : ``} \
             ${image} \
-            powershell c:/steps/entrypoint.ps1`;
+            ${entrypointBash ? `-c` : `${commandPrefix} -c`} \
+            "${overrideCommands !== '' ? overrideCommands : `/entrypoint.sh`}"`;
   }
 }
 
